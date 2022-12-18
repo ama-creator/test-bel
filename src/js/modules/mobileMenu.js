05c58cff641e170b777f0be7d7928ce576490a1d
Anatoly <markduho0@gmail.com>
2022-12-18 15:34:25 +0300

    2

---
+++ src/js/modules/mobileMenu.js
@@ -0,0 +1,20 @@
+// mobile menu
+const toggleMenu = () => {
+  const hamburgerBtn = document.querySelector(".hamburger");
+  const menulist = document.querySelector(".mobile-menu");
+
+  hamburgerBtn.addEventListener("click", (event) => {
+    hamburgerBtn.classList.toggle("is-active");
+    menulist.classList.toggle("mobile-menu--active");
+    document.querySelector("html").style.overflowY = "hidden";
+
+    if (!hamburgerBtn.classList.contains("is-active")) {
+      menulist.classList.remove("mobile-menu--active");
+      document.querySelector("html").style.overflowY = "scroll";
+    }
+  });
+};
+
+toggleMenu();
+
+export default toggleMenu;
